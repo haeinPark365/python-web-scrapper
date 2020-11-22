729bbb25e0f4edd9f73866567a9454fec08a163c
HAEIN PARK <sunman365@naver.com>
2020-11-22 13:49:02 +0000

    extract job from StackOverflow

--- so.py
+++ so.py
@@ -10,12 +10,30 @@ def get_last_page():
   last_pages = pages[-2].find("span").string
   return int(last_pages)
 
+
 def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
-    print(page)
+    request = requests.get(f"{URL}&pg={page+1}")
+    soup = BeautifulSoup(request.text,"html.parser")
+    results = soup.find_all("div",{"class":"-job"})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
+  
+  return jobs
+
+
+def extract_job(html):
+  title = html.find("h2",{"class":"mb4"}).find("a")["title"]
+  company, location = html.find("h3").find_all("span", recursive=False) # unpacking
+  #recursive는 span안에 포함된 span을 가져오지 않도록
+
+  print(company.get_text(strip=True), location.get_text(strip=True))
+  return {'title': title}
+
 
 def get_jobs():
   last_page = get_last_page()
-  jobs = extract_jobs()
+  jobs = extract_jobs(last_page)
   return jobs
